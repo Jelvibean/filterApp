a6298bab0b56df9123a861452c65510fc92f052f
SHR\yelvy.buitrago <yelvy.buitrago@stofgaming.com>
2017-03-31 13:59:40 -0400

    update variables.

--- src/assets/js/plugins.js
+++ src/assets/js/plugins.js
@@ -1,7 +1,7 @@
 window.onload = function() {
 	var one = '1';
 	var person = "Hello";
-	var person2 = "Jah";
+	var person2 = "John";
 	
 	console.log(person + person2);
 };
--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,11 +1,11 @@
 window.onload = function() {
 	var bodyHeight = document.bodyHeight;
 	var alexa = 2;
-	var yelvy = 3;
-	var jah = 5;
+	var jane = 3;
+	var John = 5;
 	var cali = 6;
 	
-    document.innerHTML = alexa + yelvy + jah + cali + bodyHeight;
+    document.innerHTML = alexa + Jane + jah + cali + bodyHeight;
 
 };
 
