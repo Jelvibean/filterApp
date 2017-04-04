a4a05f7dfad8b2c17ba4875c411c01a4b0489b0e
SHR\yelvy.buitrago <yelvy.buitrago@stofgaming.com>
2017-04-04 16:34:52 -0400

    adding bower, handlerbars and jquery to project.

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,11 +1,34 @@
-window.onload = function() {
-	var bodyHeight = document.bodyHeight;
-	var alexa = 2;
-	var jane = 3;
-	var John = 5;
-	var cali = 6;
-	
-    document.innerHTML = alexa + Jane + jah + cali + bodyHeight;
+(function(){
+	var content = document.getElementById('content');
+	var html = '';
+	var data = {
+		title: 'Really Cool People',
+		people: [
+			{
+				name: 'John',
+				age: 34,
+				gender: 'male'
+			},
+			{
+				name: 'Jane',
+				age: 23,
+				gender: 'female'
+			},
+						{
+				name: 'Betty',
+				age: 28,
+				gender: 'female'
+			},
+			{
+				name: 'Carl',
+				age: 45,
+				gender: 'male'
+			}
+		]
+	};
 
-};
+	var template = Handlebars.compile(document.getElementById('people-template').innerHTML);
 
+	content.innerHTML = template(content);
+
+})();
